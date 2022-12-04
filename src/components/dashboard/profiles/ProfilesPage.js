de94ab246ac7fe1d21606d5bf89bf9d8be4644dc
KThomiss <79108090+KThomiss@users.noreply.github.com>
2022-12-04 12:26:34 +0100

    larger width to intro container

--- src/components/dashboard/profiles/ProfilesPage.js
+++ src/components/dashboard/profiles/ProfilesPage.js
@@ -6,7 +6,7 @@ export default function ProfilesPage() {
   document.title = "Profiles | ToAd";
   return (
     <div className="theme-page-container px-3">
-      <div className="intro-container">
+      <div className="intro-container profiles-intro-container">
         <Heading title="User Profiles" />
         <div className="d-flex justify-content-between">
           <Heading size={2} title="View profile to see their posts and activites." />
